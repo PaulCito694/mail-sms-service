04126dfc99d2a39e599ae1957341eff16af4e27e
Paul Ramirez <paulr1604@gmail.com>
2023-02-01 15:16:09 -0500

    Primer commit

---
+++ src/main/java/com/smartsoft/ShopCar/ShopCarApplication.java
@@ -0,0 +1,13 @@
+package com.smartsoft.ShopCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopCarApplication.class, args);
+	}
+
+}
