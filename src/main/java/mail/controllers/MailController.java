309a0aeab892736c62e591f3092dd2606a02a8e8
Paul Ramirez <paulr1604@gmail.com>
2023-02-01 16:05:03 -0500

    creacion de mail-service y RestController funcionando

---
+++ src/main/java/mail/controllers/MailController.java
@@ -0,0 +1,16 @@
+package mail.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mail")
+public class MailController {
+
+    @PostMapping
+    public ResponseEntity<Character> sendMessage() {
+        return  ResponseEntity.ok('s');
+    }
+}
